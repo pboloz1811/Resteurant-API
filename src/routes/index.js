c389a251dd53e2134e61ed729189f5562938e8fd
Patryk <patryk@MacBook-Pro-Patryk.local>
2017-02-06 17:32:26 +0100

    resteurant api

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+import restaurant from '../controller/restaurant'
+import mongoose from 'mongoose';
+let router = express();
+
+//connect db
+
+initializeDb(db => {
+
+  router.use(middleware({config,db}));
+
+  router.use('/restaurant', restaurant({config, db}));
+
+});
+
+export default router;
